f960d4e67908dbbaf9250e0913ca5145c0c76482
Adam Alvarado <d3ltcod@protonmail.com>
2021-10-14 14:58:49 +0200

    Added some tests for appointments controller

---
+++ spec/requests/appointments_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+RSpec.describe "Appointments", type: :request do
+  describe "GET /index" do
+    context "user is not authenticated" do
+      it "has status redirect" do
+        get appointments_url
+        expect(response).to have_http_status(302)
+      end
+      
+      it "redirect to the login page" do
+        get appointments_url
+        expect(response).to redirect_to('/users/sign_in')
+      end
+    end
+
+    context "user is authenticated" do
+      before do
+        user = FactoryBot.build :user
+        user.save
+  
+        sign_in user
+      end
+  
+      it "returns a sucessful status" do
+        get appointments_url
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+  describe "GET /show/:id" do
+    context "user is not authenticated" do
+      it "has status redirect" do
+        get appointment_url('1')
+        expect(response).to have_http_status(302)
+      end
+      
+      it "redirect to the login page" do
+        get appointment_url('1')
+        expect(response).to redirect_to('/users/sign_in')
+      end
+    end
+
+    context "user is authenticated" do
+      before do
+        @user = FactoryBot.build :user
+        @user.save
+  
+        sign_in @user
+      end
+  
+      it "returns an error not found if appointment does not exist" do
+        get appointment_url('1')
+        expect(response).to have_http_status(404)
+      end
+
+      it "returns a sucessful status" do
+        patient = Patient.find(@user.id)
+        appointment = FactoryBot.build :appointment, patient: patient
+        appointment.save
+
+        patient.appointments << appointment
+        patient.save
+
+        get appointment_url('1')
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+end
--- spec/requests/patients_spec.rb
+++ spec/requests/patients_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Appointments", type: :request do
+RSpec.describe "Patients", type: :request do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
