795cc43c6b782819b776c40127f28789380c422a
Adam Alvarado <d3ltcod@protonmail.com>
2021-10-14 18:12:59 +0200

    Added tests for directory requests

--- spec/requests/directories_spec.rb
+++ spec/requests/directories_spec.rb
@@ -2,6 +2,38 @@ require 'rails_helper'
 
 RSpec.describe "Directory", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    context "user is not authenticated" do
+      it "has status redirect" do
+        get directory_url
+        expect(response).to have_http_status(302)
+      end
+      
+      it "redirect to the login page" do
+        get directory_url
+        expect(response).to redirect_to('/users/sign_in')
+      end
+    end
+
+    context "user is authenticated" do
+      before do
+        @user = FactoryBot.build :user
+        @user.save
+  
+        sign_in @user
+      end
+  
+      it "returns a sucessful status" do
+        get directory_url
+        expect(response).to have_http_status(200)
+      end
+
+      it "returns a 401 status if user is not a patient" do
+        @user.role="doctor"
+        @user.save
+  
+        get directory_url
+        expect(response).to have_http_status(401)
+      end
+    end
   end
 end
