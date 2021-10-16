e0a1362b99fd804d2e662f4db299ff339991b942
Adam Alvarado <d3ltcod@protonmail.com>
2021-10-16 12:14:41 +0200

    Added notifications

--- app/javascript/controllers/app.js
+++ app/javascript/controllers/app.js
@@ -1,9 +1,22 @@
 export default class AppCtrl {
+
+  constructor() {
+    this.notifications()
+  }
+
   back() {
     window.history.back()
   }
 
   signOut(id) {
     ajax('/users/sign_out', { params: {id: id}, options: { method: 'DELETE' }})
+  }
+
+  notifications() {
+    setTimeout(() => {
+      findAll('.notification').forEach(el => {
+        el.style.visibility = "hidden";
+      })
+    }, 1500)
   }
 }
